3da82daab8879ead0e858572500851062baeb55a
Steven Conaway <steven.conaway@icloud.com>
2020-04-16 14:14:05 -0700

    Add Documentation to Example; Prepare heading for GUI

--- FakeRobot/FakeRobot.py
+++ FakeRobot/FakeRobot.py
@@ -3,10 +3,10 @@ from time import sleep
 
 
 class FakeRobot:
-    speed = 1
+    speed = 50
     position = (0, 0, 0)
     positions = [(0, 0, 0)]
-    heading = 0
+    _heading = 0
 
     sleep(0)
 
@@ -14,22 +14,27 @@ class FakeRobot:
         """ Constructor for this class. """
         # do nothing
 
+    def set_heading(self, angle):
+        """Add angle to the current heading of the robot"""
+        self._heading = angle
+
     def turn(self, angle):
-        self.heading += angle
+        """Add angle to the current heading of the robot"""
+        self.set_heading(self._heading + angle)
 
     def forward(self, distance):
         i = 0
 
         while True:
-            # sleep(1 * 0.01)
+            sleep(1 * 0.01)
             i = i + 1
             if i * self.speed * 0.01 >= distance:
                 break
 
         self.position = (
-            self.position[0] + round(math.cos(math.radians(self.heading)) * distance, 2),
-            self.position[1] + round(math.sin(math.radians(self.heading)) * distance, 2),
-            self.heading
+            self.position[0] + round(math.cos(math.radians(self._heading)) * distance, 2),
+            self.position[1] + round(math.sin(math.radians(self._heading)) * distance, 2),
+            self._heading
         )
 
         print(self.position)
@@ -39,15 +44,15 @@ class FakeRobot:
     def backward(self, distance):
         i = 0
         while True:
-            # sleep(1 * 0.01)
+            sleep(1 * 0.01)
             i = i + 1
             if i * self.speed * 0.01 >= distance:
                 break
 
         self.position = (
-            self.position[0] - round(math.cos(math.radians(self.heading)) * distance, 2),
-            self.position[1] - round(math.sin(math.radians(self.heading)) * distance, 2),
-            self.heading
+            self.position[0] - round(math.cos(math.radians(self._heading)) * distance, 2),
+            self.position[1] - round(math.sin(math.radians(self._heading)) * distance, 2),
+            self._heading
         )
 
         print(self.position)
--- example.py
+++ example.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Import FakeRobot
 from FakeRobot import FakeRobot
 
+# Create Robot
 robot = FakeRobot()
 
+# Set speed of robot. Speed is between 1 and 100
 robot.speed = 2
 
+# Turn robot by x degrees.
 robot.turn(45)
+# Alternatively,
+# robot.set_heading(-34)
+# to set an absolute heading
 
+# Move the robot forward x units
 robot.forward(2)
 
+# Turn robot by x degrees.
 robot.turn(45)
+# Alternatively,
+# robot.set_heading(-34)
+# to set an absolute heading
 
+# Move the robot backward x units
 robot.backward(2)
 
+# Print a list of all its positions
 print(robot.positions)
